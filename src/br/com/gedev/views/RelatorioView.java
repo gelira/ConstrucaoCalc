3b6d76ac44dffc82ed7d2c46392909284e296348
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-01-10 21:17:15 -0300

    view de relatório

---
+++ src/br/com/gedev/views/RelatorioView.java
@@ -0,0 +1,27 @@
+package br.com.gedev.views;
+
+import br.com.gedev.domain.Comodo;
+import br.com.gedev.domain.TipoPiso;
+
+import java.util.List;
+
+public class RelatorioView {
+    public static void relatorioPorTipoPiso(TipoPiso tipo, List<Comodo> comodos) {
+        double areaTotal = 0;
+
+        System.out.printf("Cômodos com piso de %s%n", tipo.getNomeTipo());
+
+        for (Comodo comodo : comodos) {
+            double area = comodo.getArea();
+            System.out.printf("Cômodo: %s%nÁrea: %.2f m²%n%n", comodo.getNomeComodo(), area);
+
+            areaTotal += area;
+        }
+
+        System.out.printf("Área total com piso de %s: %.2f%n%n", tipo.getNomeTipo(), areaTotal);
+    }
+
+    public static void relatorioAreaTotal(double areaTotal) {
+        System.out.printf("A quantidade total de revestimento a ser adquirida é %.2f%n", areaTotal);
+    }
+}
